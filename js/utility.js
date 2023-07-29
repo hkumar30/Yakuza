0e9d63322631e5c62191efb60e8b3e2646e48cd8
hkumar30 <hkumar30@asu.edu>
2023-07-28 21:19:47 -0700

    Initial commit

---
+++ js/utility.js
@@ -0,0 +1,42 @@
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+    document.querySelector('#resultText').style.display = 'flex'
+  
+    if (player.health === enemy.health) {
+      document.querySelector('#resultText').innerHTML = 'Tie'
+    }
+  
+    else if (player.health > enemy.health) {
+      document.querySelector('#resultText').innerHTML = 'Player 1 Wins'
+    }
+  
+    else {
+      document.querySelector('#resultText').innerHTML = 'Player 2 Wins'
+    }
+  }
+
+function collide({ rectangle1, rectangle2 }) {
+    return (
+      rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
+      && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
+      && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+      && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+  }
+
+let timer = 60;
+let timerId;
+function decreaseTimer() {
+    if (timer > 0) {
+      timerId = setTimeout(decreaseTimer, 1000)
+      timer--
+      document.querySelector('#timer').innerHTML = timer
+    }
+  
+    if (timer === 0) {
+      determineWinner({player, enemy, timerId});
+    }
+  
+  }
+
+  decreaseTimer()
